6afb66136f8495c8b27987821cff4401e9339a95
Antonio Rios <antonio.riosm@gmail.com>
2017-08-19 02:17:20 -0500

    1er req: El buscador muestra todos los registros de la data.json al hacer click en el botón “Mostrar todos”.

--- public/app.js
+++ public/app.js
@@ -1,3 +1,45 @@
+'use strict';
+
+$(function () {
+
+  $('#buscar').on('click', function () {
+
+    $.getJSON('data.json', (data) => {
+      $('.lista').empty();
+      $.each(data, (indice, valor) => {
+        let card = `<div class="card horizontal">
+          <div class="card-image">
+            <img src="img/home.jpg">
+          </div>
+          <div class="card-stacked">
+            <div class="card-content">
+              <div>
+                <b>Direccion: </b><span>${valor.Direccion}</span>
+              </div>
+              <div>
+                <b>Ciudad: </b><span>${valor.Ciudad}</span>
+              </div>
+              <div>
+                <b>Telefono: </b><span>${valor.Telefono}</span>
+              </div>
+              <div>
+                <b>Código postal: </b><span>${valor.Codigo_Postal}</span>
+              </div>
+              <div>
+                <b>Precio: </b><span>${valor.Precio}</span>
+              </div>
+              <div>
+                <b>Tipo: </b><span>${valor.Tipo}</span>
+              </div>
+            </div>
+          </div>
+        </div>`;
+        $('.lista').append(card);
+      });
+    });
+  });
+});
+
 //Inicializador del elemento Slider
 $("#rangoPrecio").ionRangeSlider({
   type: "double",
@@ -7,18 +49,18 @@ $("#rangoPrecio").ionRangeSlider({
   from: 1000,
   to: 20000,
   prefix: "$"
-})
+});
 
 function setSearch() {
-  let busqueda = $('#checkPersonalizada')
+  let busqueda = $('#checkPersonalizada');
   busqueda.on('change', (e) => {
     if (this.customSearch == false) {
-      this.customSearch = true
+      this.customSearch = true;
     } else {
-      this.customSearch = false
+      this.customSearch = false;
     }
-    $('#personalizada').toggleClass('invisible')
-  })
+    $('#personalizada').toggleClass('invisible');
+  });
 }
 
-setSearch()
+setSearch();
